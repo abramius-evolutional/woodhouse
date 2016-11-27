857cdb9805de7b09912b3b5f2164cba2d56cd341
Kirill Drozdov <kirdro@getshopster.com>
2016-11-27 21:48:09 +0700

    front integration

--- web/code/woodhouse/settings.py
+++ web/code/woodhouse/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'static/app/dist'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- web/code/woodhouse/views.py
+++ web/code/woodhouse/views.py
@@ -4,5 +4,5 @@ import os
 
 def main(request):
     path_to_file = os.path.join(settings.BASE_DIR, settings.STATIC_ROOT)
-    path_to_file = os.path.join(path_to_file, 'app/index.html')
+    path_to_file = os.path.join(path_to_file, 'index.html')
     return HttpResponse(open(path_to_file).read())
